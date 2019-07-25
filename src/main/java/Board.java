4c0c55ee62bec2d0e23bce22f1814ee1280fdae7
Damián Pumar <damianpumar@gmail.com>
2019-07-25 10:57:14 +0200

    Refactor design

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,28 +1,55 @@
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Board {
-    private final Players players;
+    private final ArrayList<Player> players;
     private final Score score;
 
-    public Board(Random randomRoll) {
-        this.players = new Players(randomRoll);
+    private final Random randomRoll;
+    private final CategorySelector categorySelector;
+    private final QuestionSelector questionSelector;
 
+    private int currentPlayer = 0;
+
+    public Board(Random randomRoll) {
+        this.players = new ArrayList();
         this.score = new Score();
+
+        this.randomRoll = randomRoll;
+        this.categorySelector = new CategorySelector();
+        this.questionSelector = new QuestionSelector();
     }
 
     public void addPlayer(String name) {
-        players.addPlayer(name);
+        players.add(new Player(name, randomRoll, categorySelector, questionSelector));
+
+        System.out.println("They are player number " + howManyPlayers());
     }
 
     public void roll() {
-        players.roll();
+        player().roll();
+    }
+
+    public void changePlayer() {
+        currentPlayer++;
+
+        if (currentPlayer == howManyPlayers())
+            currentPlayer = 0;
     }
 
     public boolean isWinnerCurrentPlayer() {
-        Boolean isWinner = this.score.isWinner(players.player());
+        Boolean isWinner = this.score.isWinner(player());
 
-        players.changePlayer();
+        changePlayer();
 
         return isWinner;
+    }
+
+    private Player player() {
+        return players.get(currentPlayer);
+    }
+
+    private int howManyPlayers() {
+        return players.size();
     }
 }
